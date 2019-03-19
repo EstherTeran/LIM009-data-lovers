0723a18f46125f3281b4397ea88e8ab41967e3d7
Esthertc <teranesther97@outlook.es>
2019-03-18 23:04:11 -0500

    muestra151pokemonesenlaconsola

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 /* Manejo del DOM */
-console.log(POKEMON.pokemon[0]);
+/*console.log(POKEMON.pokemon[1]);*/
+function mostrarPokemones(pokemon){
+for(let i=0;i<pokemon.length;i++){
+console.log(pokemon[i])
+}
+};
+mostrarPokemones(POKEMON.pokemon)
