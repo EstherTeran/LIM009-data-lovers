d5268fda1e655936bdc8a330c7d9b7276169e841
DaaDaniela <danieladelcarmen168@hotmail.com>
2019-03-22 15:28:43 -0500

    Mostrando pokemones e informacion en la web

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 /* Manejo del DOM */
-<<<<<<< HEAD
 
 const data = POKEMON.pokemon;
 const root = document.getElementById('root');
@@ -19,23 +18,3 @@ for(let i=0;i<data.length;i++){
    Debilidades: ${data[i].weaknesses}
  </div> </div>
   `}
-=======
-//const fulltype =data.pokemon.map(pokemon=>`${pokemon.name}`);
-//console.log(fulltype)
-const todo = POKEMON.pokemon.map(pokemon=>"Numero:" +`${pokemon.num}  \n Nombre: ${pokemon.name} \n Tipo: ${pokemon.type} \n Altura: ${pokemon.height} \n Peso: ${pokemon.weight} \n Caramelos: ${pokemon.candy} \n Huevos: ${pokemon.egg} \n Debilidades: ${pokemon.weaknesses} `);
-//console.log(todo)
-for  ( i=0; i<todo.length; i++){
-  console.log(todo[i])
-}
-//const mostrar = POKEMON.pokemon;
-
-// for  ( i=0; i<mostrar.length; i++)
-// console.log(mostrar[i].img);
-/*console.log(POKEMON.pokemon[1]);*/
-function mostrarPokemones(pokemon){
-for(let i=0;i<pokemon.length;i++){
-console.log(pokemon[i])
-}
-};
-mostrarPokemones(POKEMON.pokemon)
->>>>>>> 0aea4189d6676e15ca9d0e7d428fb061e46f90a9
