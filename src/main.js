0d36b02d2679ec6e31eb3a996c3a1d81c0a32a34
DaaDaniela <danieladelcarmen168@hotmail.com>
2019-04-02 12:38:11 -0500

    Implementando ordenamiento de A-Z

--- src/main.js
+++ src/main.js
@@ -1,17 +1,46 @@
+/* Manejo del DOM */
 const data = POKEMON.pokemon;
 const root = document.getElementById('root');
 for(let i=0;i<data.length;i++){
-  root.innerHTML += `
-   <div class = pokes>
-   <div> <img src="${data[i].img} " alt="${data[i].name} "></img>
-    </div>
+ root.innerHTML += `
+  <div class="column-4 pokemon-class">
+  <div class="center"> <img src="${data[i].img} " alt="${data[i].name} "></img>
+   </div>
+   <div class="color">
+  
+    Nombre:${data[i].name} </br>
+  Tipo: ${data[i].type} </br>
+  Altura: ${data[i].height} </br> 
+  Peso: ${data[i].weight} </br>
+    Huevos: ${data[i].egg} </br> 
+ 
+</div> </div>`}
+
+btnAZ.addEventListener("click", function() {
+  root.innerHTML = '';
+  data.sort(function(a,b){
+    if(a.name > b.name)
+      return 1;
+    if(a.name < b.name)
+      return -1;
+    return 0;
+  }); 
+  for (let i=0; i<data.length; i++) {
+    root.innerHTML += `
     <div>
-     <p> Nombre:${data[i].name} </p>
-     <p> Tipo: ${data[i].type} </p> 
-     <p> Altura: ${data[i].height} </p>  
-     <p> Peso: ${data[i].weight} </p> 
-     <p> Huevos: ${data[i].egg} </p> 
- </div> </div>
- `}
+    <div> <img src="${data[i].img} " alt="${data[i].name} "></img>
+     </div>
+    <div>
+    Nombre:${data[i].name} </br>
+  Tipo: ${data[i].type} </br> 
+Altura: ${data[i].height} </br>  
+  Peso: ${data[i].weight} </br> 
+</div>`;
+  }
+});
+
+
+
+
 
  
