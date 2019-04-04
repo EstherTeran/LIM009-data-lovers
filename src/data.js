e74fad09a2105f8e0fde74887083bd5637b20b3f
DaaDaniela <danieladelcarmen168@hotmail.com>
2019-04-04 10:22:26 -0500

    agregando y ordenando codigo

--- src/data.js
+++ src/data.js
@@ -1,10 +1,54 @@
 /* Manejo de data */
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+const filterData=(data,condition)=>{
+let nuevosPokemones = [];
 
-const example = () => {
-  return 'example';
-};
+    for(let i=0;i<data.length;i++){
 
-window.example = example;
+      let x = data[i].type;
+      
+      for(let j=0;j<x.length;j++){
+      
+      if(x[j]===condition){
+
+      nuevosPokemones.push(data[i]);
+        
+
+     return nuevosPokemones;
+    
+}
+      }
+    }
+  }
+  window.filterData= filterData;
+
+
+  const sortDataAZ = (a,b) => {
+    if(a.name < b.name)
+      return 1;
+    if(a.name > b.name)
+      return -1;
+    return 0;
+  }
+  window.sortDataAZ= sortDataAZ;
+
+  const sortDataZA = (a,b) => {
+    if(a.name > b.name)
+      return 1;
+    if(a.name < b.name)
+      return -1;
+    return 0;
+  }
+  window.sortDataZA= sortDataZA;
+
+const average = () => {
+  let nuevoArray=[];
+ for(let i=0; i<data.length; i++){
+ copy.push(parseInt((data[i].weight).slice(0,-2)));
+ }
+ 
+ let suma=(new Function("return" + nuevoArray.join("+")))();
+
+ let media= suma/nuevoArray.length;
+}
+window.average = average;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-/* Manejo del DOM */
-const data = POKEMON.pokemon;
 const root = document.getElementById('root');
+const data = POKEMON.pokemon;
+
+const paint=(data)=>{
 for(let i=0;i<data.length;i++){
  root.innerHTML += `
   <div class="column-4 pokemon-class">
@@ -15,32 +16,13 @@ for(let i=0;i<data.length;i++){
     Huevos: ${data[i].egg} </br> 
  
 </div> </div>`}
-
-btnAZ.addEventListener("click", function() {
-  root.innerHTML = '';
-  data.sort(function(a,b){
-    if(a.name > b.name)
-      return 1;
-    if(a.name < b.name)
-      return -1;
-    return 0;
-  }); 
-  for (let i=0; i<data.length; i++) {
-    root.innerHTML += `
-    <div>
-    <div> <img src="${data[i].img} " alt="${data[i].name} "></img>
-     </div>
-    <div>
-    Nombre:${data[i].name} </br>
-  Tipo: ${data[i].type} </br> 
-Altura: ${data[i].height} </br>  
-  Peso: ${data[i].weight} </br> 
-</div>`;
-  }
-});
-
-
+}
+paint(data);
 
 
 
  
+
+
+
+
