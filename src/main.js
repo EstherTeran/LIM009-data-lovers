11ee22a7043eb51db4da9d87dfbfb3292385767f
EstherTeran <teranesther97@outlook.es>
2019-04-04 23:27:18 -0500

    crea funcion filtrarPokemons

--- src/main.js
+++ src/main.js
@@ -1,30 +1,34 @@
-/* Manejo del DOM */
-
 const data = POKEMON.pokemon;
 const root = document.getElementById('root');
-for(let i=0;i<data.length;i++){
-  root.innerHTML += `
-   <div class="alert alert-primary">
-   <div> <img src="${data[i].img} " alt="${data[i].name} "></img>
-   <div class="card mb-3" style="max-width: 540px;">
-  <div class="row no-gutters">
-    <div class="col-md-4">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">${data[i].name}</h5>
-        <p class="card-text">Numero:${data[i].num} </br>
-      Tipo: ${data[i].type} </br> 
-    Altura: ${data[i].height} </br>  
-      Peso: ${data[i].weight} </br> 
-      Caramelos: ${data[i].candy} </br>  
-       Huevos: ${data[i].egg} </br>  
-      Debilidades: ${data[i].weaknesses} </p>
-        
+const selecType=document.getElementById("selecType");
+
+selecType.addEventListener("change",()=>{
+  root.innerHTML="";
+if(selecType.value==="Todos"){
+root.innerHTML=printPokemons(data)
+}
+else{
+  const dataFiltrada=window.filterPokemons(data, selecType.value);
+  root.innerHTML=printPokemons(dataFiltrada);
+}
+});
+
+
+const printPokemons = (pokemons) => {
+  let template = " ";
+  for (let i = 0; i < pokemons.length; i++) {
+    template += `
+     <div class="pokemoncitos" >
+     <div > <img src="${pokemons[i].img} " alt="${pokemons[i].name} "></img>
       </div>
-    </div>
-  </div>
-</div>
-    </div>
-    </div>
-  `}
+      <div class="color">  
+       Nombre:${pokemons[i].name} </br>
+     Tipo: ${pokemons[i].type} </br> 
+     Altura: ${pokemons[i].height} </br>  
+     Peso: ${pokemons[i].weight} </br> 
+    Huevos: ${pokemons[i].egg} </br>  
+   </div> </div>
+   `}
+  return template;
+}
+root.innerHTML=printPokemons(data);
